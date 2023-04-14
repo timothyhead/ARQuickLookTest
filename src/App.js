56cf7ff591eac348e222cc94af79636d366a79c3
timothyhead <48153209+timothyhead@users.noreply.github.com>
2023-04-14 14:49:49 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import cube from './Untitled.usdz';
 function App() {
   return (
     <div className="App">
-<h1>Hello</h1>
+<h1>Hello world</h1>
 
 <a  rel="ar" href={cube + 'applePayButtonType=plain'}>  <img src={chicken}></img></a>
     </div>
