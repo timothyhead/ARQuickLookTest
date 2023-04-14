d086189b8e535d7ca6e1a9f294417d308d325eb0
timothyhead <48153209+timothyhead@users.noreply.github.com>
2023-04-14 16:05:30 +0100

    fourth commit of new project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import chicken from './chicken.jpg';
 import cube from './Untitled.usdz';
 import './App.css';
 
+const linkElement = document.getElementById("ar-link");
+linkElement.addEventListener("message", function (event) {   
+    if (event.data == "_apple_ar_quicklook_button_tapped") {
+        // Handle the user tap.   
+        console.log("tapped");
+    }
+}, false);
+
 function App() {
   return (
     <div className="App">
 <h1>Hello</h1>
 
-<a  rel="ar" href={cube + '#applePayButtonType=plain'}>  <img src={chicken}></img></a>
+<a id="ar-link" rel="ar" href={cube + '#applePayButtonType=plain'}>  <img src={chicken}></img></a>
 <h1>Hello world</h1>
     </div>
   );
