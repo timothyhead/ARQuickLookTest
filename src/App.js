00cf968572bf5a0135218a8c83fdab7be16e277d
timothyhead <48153209+timothyhead@users.noreply.github.com>
2023-04-14 15:57:39 +0100

    second commit of new project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import chicken from './chicken.jpg';
 import cube from './Untitled.usdz';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
 <h1>Hello</h1>
 
-<a  rel="ar" href={cube + 'applePayButtonType=plain'}>  <img src={chicken}></img></a>
+<a  rel="ar" href={cube}>  <img src={chicken}></img></a>
 <h1>Hello world</h1>
     </div>
   );
